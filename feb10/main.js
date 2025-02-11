35a9ae37085a78cc8b83765809a7abd99c979a88
atgll <angelt.guillo@gmail.com>
2025-02-11 18:33:37 +0100

    cambios30211

--- feb10/main.js
+++ feb10/main.js
@@ -1,16 +1,36 @@
 let miBoton = document.getElementById("progressbutton");
 let miProgressBar = document.getElementById("progressbar");
 let moreProgressBar = 0;
+let lessButton = document.getElementById("lessButton");
 
-console.log(miBoton);
 
 
 function fullProgressBar() {
-   moreProgressBar += 10;
-   return miProgressBar.value = moreProgressBar;
+    if (moreProgressBar <= 100) {
+        moreProgressBar += 10;
+        return miProgressBar.value = moreProgressBar;
+    } else {
+        alert("The bar is full.")
+    }
+
+}
+
+function emptyProgressBar() {
+    if (moreProgressBar >= 0) {
+        moreProgressBar -= 10;
+        return miProgressBar.value = moreProgressBar
+    } else {
+        alert("The bar is empty.")
+    }
 }
 
 miBoton.addEventListener("click", function(event) {
     event.preventDefault();
     fullProgressBar();
 })
+
+lessButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    emptyProgressBar();
+})
+
