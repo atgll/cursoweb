ca0075d8ae55db1bf7906abb2cb40bc70985566c
atgll <angelt.guillo@gmail.com>
2025-02-21 01:53:48 +0100

    añadido proyecto 12 columnas

--- css 1/barrainfo/scriptbarra.js
+++ css 1/barrainfo/scriptbarra.js
@@ -21,3 +21,21 @@ $(document).ready(function () {
         cssEase: 'linear'
     });
 });
+
+/*const numbers = [1, 2, 3, 4, 5]
+
+const evenNumbers = numbers.filter(function(number){
+    return number % 2 ===0
+})
+
+console.log(evenNumbers)
+
+console.log(numbers[0])
+
+const aboveTwoNumbers = numbers.filter(function(number){
+    return number > 2
+})
+
+console.log(aboveTwoNumbers)
+
+console.log(numbers.length)*/
