bf765f4e7f19c96cff9ab7b73e3404a728ddde9d
atgll <angelt.guillo@gmail.com>
2025-04-16 18:27:34 +0200

    act mysql_papercss in cursoweb

--- mysql_papercss/scripts/articulos.js
+++ mysql_papercss/scripts/articulos.js
@@ -1,6 +1,6 @@
-export function articulosver($tipo) {
+export function articulosver($tipo, $contenedordatos) {
    //se coomprueba si el naveagdor soporta plantillas
-   if('content' in document.createElement('template')) {
+   if ('content' in document.createElement('template')) {
       //obtenr datos de los articulos segun el tipo
       //primero creamos el dato a enviar
       let datos = new FormData();
@@ -10,10 +10,19 @@ export function articulosver($tipo) {
          method: 'POST',
          body: datos
       })
-      .then(respuesta => respuesta.json())
-      .then(respuestajson => {
-         console.log(respuestajson)
-      }
-      )
+         .then(respuesta => respuesta.json())
+         .then(respuestajson => {
+            // console.log(respuestajson)
+            let contenedor = document.getElementById($contenedordatos);
+
+            for(const articulo of respuestajson) {
+               let tarjeta = plantilla_articulos_1.content.cloneNode(true);
+               tarjeta.querySelector('.card-title').textContent = articulo.nombre;
+               tarjeta.querySelector('.card-subtitle').textContent = articulo.fecha;
+               tarjeta.querySelector('.card-text').textContent = articulo.descripcion;
+               tarjeta.querySelector('img').src = "./css/img/imgarticulos/" + articulo.imagen;
+               contenedor.appendChild(tarjeta);
+            }
+         })
    }
 }
--- mysql_papercss/scripts/main.js
+++ mysql_papercss/scripts/main.js
@@ -2,5 +2,6 @@ import { articulosver } from "./articulos.js";
 
 //iniciar la pagina
 window.addEventListener('load', (evento)=>{
-   articulosver(1);
+   articulosver(1, "articulos_recientes");
+   articulosver(2, "articulos_outlet");
 })
